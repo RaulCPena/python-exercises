25a9bd29515eb5274eaebd9fbf9d3dbcc1602775
RaulCPena <raul.c.pena@gmail.com>
2020-02-20 22:17:31 -0600

    created 4.1_python_introductiopn_exercises.py

--- 4.1_python_introduction_exercises.py
+++ 4.1_python_introduction_exercises.py
@@ -1,2 +1,10 @@
-greeting = 'Hello, World'
-print(greeting)
+user_choice = input("Enter an odd integer between 1 and 50.")
+while (user_choice.isdigit() == False # keep prompting user if input is not a whole number
+      or int(user_choice) < 1 # prompt if they input less than 1
+      or int(user_choice) > 50 # prompt if input greater than 50
+      or int(user_choice) % 2 == 0): # prompt if input an even number
+    print(f"{user_choice} is nice, but we'll need an odd number between 1 and 50. ")
+    user_choice = input("Please input an odd number between 1 and 50. ")
+else:
+        if user_choice == True:
+            print("Number to skip is: " + str(user_choice))
