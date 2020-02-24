a4cdb6061da639d1c550990ee7896b71e4b69c79
RaulCPena <raul.c.pena@gmail.com>
2020-02-24 15:45:59 -0600

    created import_exercises file

---
+++ 4.6_import_exercises.py
@@ -0,0 +1,12 @@
+# import 
+
+import functions_exercises
+functions_exercises.some_word('cool')
+
+# use from to import the function directly
+from functions_exercises import is_consonant
+functions_exercises.is_consonant('jack')
+
+# use from and give the function a different name
+from functions_exercises import is_two as double_check
+double_check(3)
